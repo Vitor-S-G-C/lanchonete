724f3aed8df41b5155ad3c954c63a5b882eaf883
Vitor gamarano <vitorgamarano1@gmail.com>
2025-07-10 18:23:54 -0300

    correção de gramatica

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { Bebidas} from "./page/Bebidas";
 import { Login } from "./page/Login";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
-import { Cadastro } from "./page/cadastro";
-import { Senha } from "./page/senha";
+import { Cadastro } from "./page/Cadastro";
+import { Senha } from "./page/Senha";
 
 function App() {
-  return (
+  return (  
     <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
